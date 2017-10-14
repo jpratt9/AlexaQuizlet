545a5018b80aa148c6f3271d6cacd99b8e27be5e
John Pratt <jpratt9@gatech.edu>
2017-10-14 14:34:32 -0400

    request now in package.json

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ let client_id = 'YEkMtWaFXg';
 let secret = process.env.QUIZLET_SECRET;
 let quizlet = 'https://api.quizlet.com/2.0/'
 let url = 'https://google.com';
-
-
 let request = require('request');
-request(quizlet + `sets/415?client_id=${client_id}&whitespace=1`, function (error, response, body) {
+
+
+// request(quizlet + `sets/415?client_id=${client_id}&whitespace=1`, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       console.log(body)
+//     }
+//   })
+
+  request.delete(quizlet + `sets/233884698?client_id=${client_id}&whitespace=1`, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body) // Print the google web page.
+      console.log(body)
+    } else {
+        console.log(response.statusCode);
+        console.log(error);
     }
   })
-  
