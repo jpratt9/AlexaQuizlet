fbb3da8dcd466eab34db21afe55cd149b0259bd0
John Pratt <jpratt9@gatech.edu>
2017-10-14 15:59:51 -0400

    add oauth file, fix readme

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let client_id = 'YEkMtWaFXg';
 let secret = process.env.QUIZLET_SECRET;
 let quizlet = 'https://api.quizlet.com/2.0/'
-let url = 'https://google.com';
+let url = 'http://example.com';
 let request = require('request');
 let options = {
     json: true
 }
+
 
 
 function getSet(setId) {
@@ -13,16 +14,18 @@ function getSet(setId) {
         if (!error && response.statusCode == 200) {
           console.log(body.terms[1])
         }
-      })
+    })
 }
 
-// function search(query) {
-//     request(quizlet + `sets/${setId}?client_id=${client_id}&whitespace=1`, function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//           console.log(body)
-//         }
-//       })
-// }
-
-getSet(415);
+function search(query) {
+    request(quizlet + `sets?q=${query}&client_id=${client_id}&whitespace=1`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        } else {
+            console.log(error)
+        }
+    })
+}
+search('french');
+//getSet(415);
 
