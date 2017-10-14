b962f117b50a122ac3d4720aa492c3988385c934
John Pratt <jpratt9@gatech.edu>
2017-10-14 15:05:53 -0400

    response returning json

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ let secret = process.env.QUIZLET_SECRET;
 let quizlet = 'https://api.quizlet.com/2.0/'
 let url = 'https://google.com';
 let request = require('request');
+let options = {
+    json: true
+}
 
 
-// request(quizlet + `sets/415?client_id=${client_id}&whitespace=1`, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       console.log(body)
-//     }
-//   })
+function getSet(setId) {
+    request(quizlet + `sets/415?client_id=${client_id}&whitespace=1`, options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body.terms[1])
+        }
+      })
+}
 
-  request.delete(quizlet + `sets/233884698?client_id=${client_id}&whitespace=1`, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body)
-    } else {
-        console.log(response.statusCode);
-        console.log(error);
-    }
-  })
+// function search(query) {
+//     request(quizlet + `sets/${setId}?client_id=${client_id}&whitespace=1`, function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//           console.log(body)
+//         }
+//       })
+// }
+
+getSet(415);
+
